66dede4a54fd78a82a01df567f142a6956fe1e15
AdamMusa <adammusa2222@gmail.com>
2026-06-17 23:20:46 -0400

    Remove obsolete modular studio layout

--- views/status_text.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module RufletStudio
-  module Views
-    def status_text(page)
-      text(value: "", style: { size: 12, color: color_subtle(page) })
-    end
-  end
-end
